47f655b9b2e74a9884c74a62d6edd27a1ff6db4f
Hiduíno Domingos <hvduino@gmail.com>
2022-05-02 15:04:47 -0300

    Adjust WithActive component

--- frontend/src/app/components/User/UserResponse/DynamicComponents/Facebook/FacebookSelector/helpers/index.js
+++ frontend/src/app/components/User/UserResponse/DynamicComponents/Facebook/FacebookSelector/helpers/index.js
@@ -2,4 +2,4 @@ export { default as Hover } from './Hover';
 export * from './HoverStyle';
 export { default as icons } from './icons';
 export * from './useHover';
-export { default as withActive } from './withActive';
+export { default as withActive } from './WithActive';
